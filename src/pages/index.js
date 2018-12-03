ed5813e39b396c66ae618cf0fc459d567e3140ec
Silver <silver@Silver.local>
2018-12-03 18:50:48 +0300

    add animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn React app</h1>
-    <p>New MYGatsby site.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn React app</h1>
+        <p>New MYGatsby site</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 
